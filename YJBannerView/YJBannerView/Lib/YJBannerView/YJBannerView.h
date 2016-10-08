1ab73fd5c5266d3ec329cf4e33e66da391682d7c
YJHou <8069060@qq.com>
2016-10-08 13:59:42 +0800

    添加数据源方法

--- YJBannerView/YJBannerView/Lib/YJBannerView/YJBannerView.h
+++ YJBannerView/YJBannerView/Lib/YJBannerView/YJBannerView.h
@@ -8,6 +8,92 @@
 
 #import <UIKit/UIKit.h>
 
+/** PageControl的位置 */
+typedef NS_ENUM(NSInteger, YJBannerViewPageControlAliment) {
+    YJBannerViewPageControlAlimentCenter,
+    YJBannerViewPageControlAlimentRight,
+};
+
+/** PageControl的样式 */
+typedef NS_ENUM(NSInteger, YJBannerViewPageControlStyle) {
+    YJBannerViewPageControlStyleDefault,
+    YJBannerViewPageControlStyleAnimated,
+    YJBannerViewPageControlStyleNone
+};
+
+
+@class YJBannerView;
+
+// >>>>>>>>>>>>>> YJBannerViewDataSource >>>>>>>>>>>>>>
+@protocol YJBannerViewDataSource <NSObject>
+
+
+@optional
+/** isAuto */
+- (BOOL)bannerAutoScrollWithBannerView:(YJBannerView *)bannerView;
+/** InfiniteLoop */
+- (BOOL)bannerInfiniteLoopWithBannerView:(YJBannerView *)bannerView;
+/** Auto Scroll Time */
+- (CGFloat)bannerAutoScrollTimeWithBannerView:(YJBannerView *)bannerView;
+/** titleLabelHeight */
+- (CGFloat)bannerTitleLabelHeightWithBannerView:(YJBannerView *)bannerView;
+/** TitleTextColor */
+- (UIColor *)bannerTitleTextColorWithBannerView:(YJBannerView *)bannerView;
+/** TitleFont */
+- (UIFont *)bannerTitleFontWithBannerView:(YJBannerView *)bannerView;
+/** titleBackgroundColor */
+- (UIColor *)bannerTitleBackgroundColorWithBannerView:(YJBannerView *)bannerView;
+
+/** PageControl Dot Size */
+- (CGSize)bannerPageControlDotSizeWithBannerView:(YJBannerView *)bannerView;
+/** CurrentPageDotColor */
+- (UIColor *)bannerCurrentPageDotColorWithBannerView:(YJBannerView *)bannerView;
+/** PageDotColor */
+- (UIColor *)bannerPageDotNormalColorWithBannerView:(YJBannerView *)bannerView;
+/** PageControlBottomOffset */
+- (CGFloat)bannerPageControlBottomOffsetWithBannerView:(YJBannerView *)bannerView;
+/** PageControlRightOffset */
+- (CGFloat)bannerPageControlRightOffsetWithBannerView:(YJBannerView *)bannerView;
+/** HidesForSinglePage */
+- (BOOL)bannerHidesForSinglePageWithBannerView:(YJBannerView *)bannerView;
+/** BannerImageViewContentMode */
+- (UIViewContentMode *)bannerImageViewContentModeWithBannerView:(YJBannerView *)bannerView;
+
+@end
+
+// >>>>>>>>>>>>>> YJBannerViewDelegate >>>>>>>>>>>>>>
+@protocol YJBannerViewDelegate <NSObject>
+
+
+@end
+
+
+
 @interface YJBannerView : UIView
+
+/** PageControl的位置 Default is Center */
+@property (nonatomic, assign) YJBannerViewPageControlAliment pageControlAliment;
+
+/** PageControl的样式 Default is 经典 */
+@property (nonatomic, assign) YJBannerViewPageControlStyle pageControlStyle;
+
+/** DataSource */
+@property (nonatomic, weak) id<YJBannerViewDataSource> dataSource;
+
+/** Delegate */
+@property (nonatomic, weak) id<YJBannerViewDelegate> delegate;
+
+
+
+
+/** 初始化方法 */
++ (instancetype)bannerViewWithFrame:(CGRect)frame placeholderImage:(UIImage *)placeholderImage;
+
+
+
+
+
+
+
 
 @end
--- YJBannerView/YJBannerView/Lib/YJBannerView/YJBannerView.m
+++ YJBannerView/YJBannerView/Lib/YJBannerView/YJBannerView.m
@@ -8,14 +8,182 @@
 
 #import "YJBannerView.h"
 
+@interface YJBannerView ()
+
+@property (nonatomic, strong) UIImage * placeholderImage; /**< 占位图，用于网络未加载到图片时 */
+
+@end
+
 @implementation YJBannerView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self _initialization];
+        [self _setUpBannerMianView];
+    }
+    return self;
 }
-*/
+
+- (void)awakeFromNib{
+    [super awakeFromNib];
+    [self _initialization];
+    [self _setUpBannerMianView];
+}
+
+
+- (void)_initialization{
+    
+    _pageControlAliment = YJBannerViewPageControlAlimentCenter;
+    _pageControlStyle = YJBannerViewPageControlStyleDefault;
+    self.backgroundColor = [UIColor lightGrayColor];
+}
+
+
++ (instancetype)bannerViewWithFrame:(CGRect)frame placeholderImage:(UIImage *)placeholderImage{
+    
+    YJBannerView * bannerView = [[YJBannerView alloc] initWithFrame:frame];
+    bannerView.placeholderImage = placeholderImage;
+    return bannerView;
+    
+}
+
+- (void)_setUpBannerMianView{
+
+}
+
+#pragma mark - 数据设置Getter
+/** isAuto Scroll Default is YES */
+- (BOOL)_isAutoScroll{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerAutoScrollWithBannerView:)]) {
+        BOOL isAutoScroll = [self.dataSource bannerAutoScrollWithBannerView:self];
+        return isAutoScroll;
+    }
+    return YES;
+}
+
+/** infiniteLoop Default is YES */
+- (BOOL)_isInfiniteLoop{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerInfiniteLoopWithBannerView:)]) {
+        BOOL isInfiniteLoop = [self.dataSource bannerInfiniteLoopWithBannerView:self];
+        return isInfiniteLoop;
+    }
+    return YES;
+}
+
+/** AutoTime Default is 2s */
+- (CGFloat)_autoScrollTimeInterval{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerAutoScrollTimeWithBannerView:)]) {
+        CGFloat autoScrollTimeInterval = [self.dataSource bannerAutoScrollTimeWithBannerView:self];
+        return autoScrollTimeInterval;
+    }
+    return 2.0f;
+}
+
+/** titleLabelHeight */
+- (CGFloat)_titleLabelHeight{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerTitleLabelHeightWithBannerView:)]) {
+        CGFloat titleLabelHeight = [self.dataSource bannerTitleLabelHeightWithBannerView:self];
+        return titleLabelHeight;
+    }
+    return 30.0f;
+}
+
+/** titleTextColor */
+- (UIColor *)_titleTextColor{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerTitleTextColorWithBannerView:)]) {
+        UIColor * titleTextColor = [self.dataSource bannerTitleTextColorWithBannerView:self];
+        if (titleTextColor) NSAssert([titleTextColor isKindOfClass:[UIColor class]], @"You must return a valid UIColor object for -bannerTitleTextColorWithBannerView:");
+        return titleTextColor;
+    }
+    return [UIColor whiteColor];
+}
+
+/** titleFont */
+- (UIFont *)_titleFont{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerTitleFontWithBannerView:)]) {
+        UIFont * titleFont = [self.dataSource bannerTitleFontWithBannerView:self];
+        if (titleFont) NSAssert([titleFont isKindOfClass:[UIFont class]], @"You must return a valid UIFont object for -bannerTitleFontWithBannerView:");
+        return titleFont;
+    }
+    return [UIFont systemFontOfSize:14];
+}
+
+/** titleBackgroundColor */
+- (UIColor *)_titleBackgroundColor{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerTitleBackgroundColorWithBannerView:)]) {
+        UIColor * titleBackgroundColor = [self.dataSource bannerTitleBackgroundColorWithBannerView:self];
+        if (titleBackgroundColor) NSAssert([titleBackgroundColor isKindOfClass:[UIColor class]], @"You must return a valid UIColor object for -bannerTitleBackgroundColorWithBannerView:");
+        return titleBackgroundColor;
+    }
+    return [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
+}
+
+/** pageControl Size */
+- (CGSize)_pageControlDotSize{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerPageControlDotSizeWithBannerView:)]) {
+        CGSize pageControlDotSize = [self.dataSource bannerPageControlDotSizeWithBannerView:self];
+        return pageControlDotSize;
+    }
+    return CGSizeMake(10, 10);
+}
+
+/** _currentPageDotColor */
+- (UIColor *)_currentPageDotColor{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerCurrentPageDotColorWithBannerView:)]) {
+        UIColor * currentPageDotColor =  [self.dataSource bannerCurrentPageDotColorWithBannerView:self];
+        if (currentPageDotColor) NSAssert([currentPageDotColor isKindOfClass:[UIColor class]], @"You must return a valid UIColor object for -bannerCurrentPageDotColorWithBannerView:");
+        return currentPageDotColor;
+    }
+    return [UIColor whiteColor];
+}
+
+/** _pageDotNormalColor */
+- (UIColor *)_pageNormalDotColor{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerPageDotNormalColorWithBannerView:)]) {
+        UIColor * pageNormalDotColor = [self.dataSource bannerPageDotNormalColorWithBannerView:self];
+        if (pageNormalDotColor) NSAssert([pageNormalDotColor isKindOfClass:[UIColor class]], @"You must return a valid UIColor object for -bannerPageDotNormalColorWithBannerView:");
+        return pageNormalDotColor;
+    }
+    return [UIColor lightGrayColor];
+}
+
+/** _pageControlBottomOffset */
+- (CGFloat)_pageControlBottomOffset{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerPageControlBottomOffsetWithBannerView:)]) {
+        return [self.dataSource bannerPageControlBottomOffsetWithBannerView:self];
+    }
+    return 0.0f;
+}
+
+/** PageControlRightOffset */
+- (CGFloat)_pageControlRightOffset{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerPageControlRightOffsetWithBannerView:)]) {
+        return [self.dataSource bannerPageControlRightOffsetWithBannerView:self];
+    }
+    return 0.0f;
+}
+
+/** hidesForSinglePage */
+- (BOOL)_hidesForSinglePage{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerHidesForSinglePageWithBannerView:)]) {
+        return [self.dataSource bannerHidesForSinglePageWithBannerView:self];
+    }
+    return YES;
+}
+
+/** _bannerImageViewContentMode */
+- (UIViewContentMode *)_bannerImageViewContentMode{
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(bannerImageViewContentModeWithBannerView:)]) {
+        return [self.dataSource bannerImageViewContentModeWithBannerView:self];
+    }
+    return UIViewContentModeScaleToFill;
+}
+
+
+
+
+
+
 
 @end
